32070e412d5fc1aded98451d1681e93e0fe427d2
lam.trinh <lam.trinh@finviet.com.vn>
2024-03-14 21:10:52 +0700

    feat: :sparkles: error with detail

--- order-service/internal/application/api/api.go
+++ order-service/internal/application/api/api.go
@@ -1,8 +1,14 @@
 package api
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/lamtrinh/go-ecom-hexagon/order-service/internal/application/domain"
 	"github.com/lamtrinh/go-ecom-hexagon/order-service/internal/ports"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Application struct {
@@ -25,7 +31,28 @@ func (a *Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 
 	paymentErr := a.payment.Charge(&order)
 	if paymentErr != nil {
-		return domain.Order{}, paymentErr
+		st, _ := status.FromError(paymentErr)
+		fmt.Printf("%+v\n", st.Details())
+		var allErrors []string
+		for _, detail := range st.Details() {
+			switch t := detail.(type) {
+			case *errdetails.BadRequest:
+				for _, violation := range t.GetFieldViolations() {
+					allErrors = append(allErrors, violation.GetDescription())
+				}
+			}
+		}
+
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: strings.Join(allErrors, "\n"),
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDetail, _ := orderStatus.WithDetails(badReq)
+		
+		return domain.Order{}, statusWithDetail.Err()
 	}
 
 	return order, nil
--- payment-service/internal/adapters/grpc/grpc.go
+++ payment-service/internal/adapters/grpc/grpc.go
@@ -2,12 +2,12 @@ package grpc
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/lamtrinh/go-ecom-hexagon/payment-service/internal/application/domain"
 
 	"github.com/lamtrinh/ecom-proto/go/payment"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -19,7 +19,16 @@ func (a Adapter) Create(ctx context.Context, request *payment.CreatePaymentReque
 	result, err := a.api.Charge(ctx, newPayment)
 
 	if err != nil {
-		return nil, status.New(codes.Internal, fmt.Sprintf("failed to charge, error: %v", err)).Err()
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "application",
+			Description: "failed to create payment",
+		}
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+		paymentStatus := status.New(codes.InvalidArgument, "payment creation failed")
+		statusWithDetail, _ := paymentStatus.WithDetails(badReq)
+		
+		return nil, statusWithDetail.Err()
 	}
 
 	return &payment.CreatePaymentResponse{
