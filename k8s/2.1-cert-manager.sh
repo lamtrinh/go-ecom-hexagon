bc4e6a1119275af91039874e4f01f5caa896cd17
lam.trinh <lam.trinh@finviet.com.vn>
2024-04-05 13:53:32 +0700

    feat: :sparkles: k8s deployments

---
+++ k8s/2.1-cert-manager.sh
@@ -0,0 +1,7 @@
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \ --create-namespace \
+  --version v1.14.4 \
+  --set installCRDs=true
